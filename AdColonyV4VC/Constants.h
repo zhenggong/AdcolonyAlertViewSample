2d4f6fed9b5423b02961d73da44b9247834fb4bc
zheng-gong <zheng.gong@gree.net>
2016-10-05 11:11:51 +0900

    first commit

---
+++ AdColonyV4VC/Constants.h
@@ -0,0 +1,17 @@
+//
+//  Constants.h
+//  AdColonyV4VC
+//
+//  Created by Owain Moss on 1/14/15.
+//  Copyright (c) 2015 AdColony. All rights reserved.
+//
+
+#define kAdColonyAppID  @"appbdee68ae27024084bb334a"
+#define kAdColonyZoneID @"vzf8e4e97704c4445c87504e"
+
+#define kCurrencyBalance       @"CurrencyBalance"
+#define kCurrencyBalanceChange @"CurrencyBalanceChange"
+
+#define kZoneOff     @"ZoneOff"
+#define kZoneLoading @"ZoneLoading"
+#define kZoneReady   @"ZoneReady"
